f687f2a6d5995252a24cac7f736626eeb0d2dadf
Alonso <22393172@utcancun.edu.mx>
2025-07-10 01:54:58 -0500

    Conectar con supabase

--- app/common/middleware.py
+++ app/common/middleware.py
@@ -1,19 +1,26 @@
 from fastapi import Header, HTTPException
-from app.common.settings import ACCESS_TOKEN, ADMIN_TOKEN
 from typing import Optional
+import httpx
+from app.common.settings import TOKEN_URL
 
 def verificar_acceso(authorization: Optional[str] = Header(None, include_in_schema=False)):
     if authorization is None or not authorization.startswith("Bearer "):
         raise HTTPException(status_code=401, detail="Token no enviado o mal formato")
+    
     token = authorization.split(" ")[1]
-    if token != ACCESS_TOKEN and token != ADMIN_TOKEN:
-        raise HTTPException(status_code=401, detail="Token de acceso inválido")
 
-def verificar_admin(authorization: Optional[str] = Header(None, include_in_schema=False)):
-    if authorization is None or not authorization.startswith("Bearer "):
-        raise HTTPException(status_code=403, detail="Token no enviado o mal formato")
-    token = authorization.split(" ")[1]
-    if token != ADMIN_TOKEN:
-        raise HTTPException(status_code=401, detail="Token de acceso inválido")
+    try:
+        response = httpx.get(
+            TOKEN_URL,
+            headers={"Authorization": f"Bearer {token}"},
+            timeout=10
+        )
+    except httpx.RequestError as e:
+        raise HTTPException(status_code=503, detail=f"No se pudo contactar el validador: {str(e)}")
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail="Token inválido o expirado")
+
+    return response.json()
 
 
