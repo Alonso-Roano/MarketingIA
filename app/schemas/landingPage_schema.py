ff6462fa7fa008bddd1819950a8ba2b725c3c089
Alonso <22393172@utcancun.edu.mx>
2025-07-28 18:02:16 -0500

    agregar reglas

--- app/schemas/landingPage_schema.py
+++ app/schemas/landingPage_schema.py
@@ -14,5 +14,59 @@ REGLAS_URLS_TEMPLATE = {
         "testimonio_2_avatar",
         "final_cta_primary",
         "final_cta_secondary"
-    ]
+      ],
+      "modernLanding": [
+        "hero_background",
+        "hero_cta_url_prymary",
+        "hero_cta_url_secondary",
+        "testimonio_0_avatar",
+        "testimonio_1_avatar",
+        "testimonio_2_avatar",
+        "final_cta_primary",
+        "final_cta_secondary"
+      ],
+      "minimalLanding": [
+        "hero_background",
+        "hero_cta_url_prymary",
+        "testimonio_0_avatar",
+        "final_cta_primary"
+      ],
+      "productShowcase": [
+        "hero_background",
+        "product_image_1",
+        "product_image_2",
+        "product_image_3",
+        "hero_cta_url_prymary",
+        "testimonio_0_avatar",
+        "final_cta_secondary"
+      ],
+      "serviceAgency": [
+        "hero_background",
+        "hero_cta_url_prymary",
+        "team_member_0_avatar",
+        "team_member_1_avatar",
+        "team_member_2_avatar",
+        "final_cta_primary",
+        "portfolio_item_1_image",
+        "portfolio_item_2_image"
+      ],
+      "healthCoach": [
+        "hero_background",
+        "hero_cta_url_prymary",
+        "coach_avatar",
+        "testimonial_0_avatar",
+        "testimonial_1_avatar",
+        "final_cta_primary"
+      ],
+   "educationCourse": [
+  "hero_background",
+  "hero_cta_url_primary",
+  "hero_cta_url_secondary",
+  "instructor_avatar",
+  "testimonio_0_avatar",
+  "testimonio_1_avatar",
+  "testimonio_2_avatar",
+  "final_cta_primary",
+  "final_cta_secondary"
+]
 }
